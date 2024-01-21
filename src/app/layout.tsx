92eed3dd7bcd3e67f13427b15503c2d81476f431
Morgan Sun <sunmorgan97@gmail.com>
2024-01-21 17:18:55 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chedulr",
+  title: "chedulr",
   description: "Create your own custom schedule",
 };
 
